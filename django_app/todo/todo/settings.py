2ddd8de067fe0ed8fc349787f1e7c5e439b7a91b
m-lair <56314378+m-lair@users.noreply.github.com>
2020-06-29 09:51:41 -0400

    Update settings.py

--- django_app/todo/todo/settings.py
+++ django_app/todo/todo/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a%_osl&50g#b47unun_s6b3#vk*q7m*fgmcmvp+88lxigjx4%z'
+SECRET_KEY = 
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -72,7 +73,7 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
 
 DATABASES = {
     'default': {
